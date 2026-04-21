b28ee23f236e0c3a7761a9594608f0da7a2d16f4
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-21 16:04:19 +0600

    first commit

---
+++ src/app/about/designers/page.jsx
@@ -0,0 +1,9 @@
+const page = () => {
+    return (
+        <div>
+            This is the designers page. You can edit this page at src/app/about/designers/page.jsx
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+const page = () => {
+    return (
+        <div>
+            This is the about page. You can edit this page at src/app/about/page.jsx
+        </div>
+    );
+};
+
+export default page;
