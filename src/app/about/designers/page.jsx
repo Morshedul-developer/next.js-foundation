4d79c74dfe2af277f420a4bf1370e0e70e6e6e35
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-22 20:58:42 +0600

    explored about custom font adding to next.js project

--- src/app/about/designers/page.jsx
+++ src/app/about/designers/page.jsx
@@ -1,9 +1,17 @@
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: "700",
+  subsets: ["latin"],
+});
+
 const DesignersPage = () => {
-    return (
-        <div>
-            This is the designers page. You can edit this page at src/app/about/designers/page.jsx
-        </div>
-    );
+  return (
+    <div className={`${roboto.className} text-4xl`}>
+      This is the designers page. You can edit this page at
+      src/app/about/designers/page.jsx
+    </div>
+  );
 };
 
 export default DesignersPage;
